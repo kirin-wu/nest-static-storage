78a48b0c1c5016304c58fc9113d73f5f0149acd0
wukeqi <1270314454qq.com>
2024-08-12 13:58:15 +0800

    delete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [UploadModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
